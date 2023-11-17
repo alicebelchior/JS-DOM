3adae008c9e20a83ae8067a88f8574248364815b
Alice Belchior <146728292+alicebelchior@users.noreply.github.com>
2023-11-17 14:08:22 -0300

    Update script.js

--- Exercícios propostos/aula23/script.js
+++ Exercícios propostos/aula23/script.js
@@ -1,9 +1,13 @@
 btn = document.getElementById("btnCalcular");
 btn.addEventListener("click", adicionarLinha);
 
+//função para add linha
 function adicionarLinha() {
     const num = parseInt(Math.random() * (100) + 1);
+    // criando um elemento tr
     const novaLinha = document.createElement("tr");
+    // adicionando o txt de dentro da celula
     novaLinha.innerHTML = `<td>${num}</td>`;
+    // inserindo a linha criada na tabela 
     document.querySelector("#tabela1").appendChild(novaLinha);
 }
